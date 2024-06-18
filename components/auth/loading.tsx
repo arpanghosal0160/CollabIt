76c6d567d66fefabda01a25739d105fd5f88013f
Arpan Ghosal <arpanghosal0160@gmail.com>
2024-06-18 12:04:42 +0530

    org-sidebar

--- components/auth/loading.tsx
+++ components/auth/loading.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export const Loading = () => {
     return(
         <div className="h-full w-full flex flex-col justify-center items-center">
-            <Image src="/logo.svg" alt="logo" width={120} height={120} className="animate-pulse duration-700" />
+            <Image src="/logo.svg" alt="logo" width={120} height={120} className="animate-pulse duration-800" />
         </div>
     )
 }
