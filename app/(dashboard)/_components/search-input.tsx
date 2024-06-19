e52a7049d94260ac8eb76a01849d6559280db238
Arpan Ghosal <arpanghosal0160@gmail.com>
2024-06-19 09:56:44 +0530

    built search bar and invite button

---
+++ app/(dashboard)/_components/search-input.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import qs from "query-string";
+import { Search } from "lucide-react";
+import { useRouter } from "next/navigation";
+import { ChangeEvent, useCallback, useState } from "react";
+import { Input } from "@/components/ui/input";
+import debounce from 'lodash.debounce';
+
+export const SearchInput = () => {
+    const router = useRouter();
+    const [value, setValue] = useState("");
+
+    const debouncedSearch = useCallback(
+        debounce((query) => {
+            const url = qs.stringifyUrl({
+                url: "/",
+                query: { search: query },
+            }, { skipEmptyString: true, skipNull: true });
+
+            router.push(url);
+        }, 500), []
+    );
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setValue(e.target.value);
+        debouncedSearch(e.target.value);
+    };
+
+    return (
+        <div className="w-full relative">
+            <Search 
+                className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4"
+            />
+            <Input 
+                className="w-full max-w-[516px] pl-9"
+                placeholder="Search boards"
+                onChange={handleChange}
+                value={value}
+            />
+        </div>
+    );
+};
